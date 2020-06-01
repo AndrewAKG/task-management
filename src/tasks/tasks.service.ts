eddb28c9469285bae7feb9e2f946548b44f008ad
AndrewAKG <andrew.akram@callvita.com>
2020-06-01 12:04:30 +0200

    feat: get task by id, delete task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './tasks.model';
 import { v1 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
 export class TasksService {
@@ -10,12 +11,24 @@ export class TasksService {
     return this.tasks;
   }
 
-  createTask(title: string, description: string){
+  getTaskById(id: string): Task {
+    return this.tasks.find(task => task.id === id);
+  }
+
+  createTask(createTaskDto: CreateTaskDto) : Task{
+    const { title, description } = createTaskDto;
     const task: Task = {
       id: uuid(),
       title,
       description,
       status: TaskStatus.OPEN
-    }
+    };
+
+    this.tasks.push(task);
+    return task;
+  }
+
+  deleteTask(id: string): void {
+    this.tasks = this.tasks.filter(task => task.id !== id);
   }
 }
